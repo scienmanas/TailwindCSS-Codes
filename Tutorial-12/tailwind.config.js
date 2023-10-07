d55b6330b9d5b3d215dfe0e61eb17bd107d26743
Manas <99756067+scienmanas@users.noreply.github.com>
2023-10-07 21:31:13 +0530

    Add files

---
+++ Tutorial-12/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing :{
+        13 : '13px'
+      },
+      screens : {
+        xsm: "340px"
+      }
+    },
+  },
+  plugins: [],
+}
+
